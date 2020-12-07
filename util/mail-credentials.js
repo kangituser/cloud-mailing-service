e245936faee21c6f7c08ac02559ffb0577de12c6
kangituser <yehonatana@kan.org.il>
2020-12-07 15:33:18 +0200

    tipper

--- util/mail-credentials.js
+++ util/mail-credentials.js
@@ -3,12 +3,12 @@ exports.credentials = {
   port: 25,
   secure: false,
   requireTLS: true,
-  auth: {
-    user: process.env.MAIL_365_USER,
-    pass: ''
-  },
-  tls: {
-    rejectUnauthorized: false,
-    ciphers: 'SSLv3'
-  }
+  // auth: {
+  //   user: process.env.MAIL_365_USER,
+  //   pass: ''
+  // },
+  // tls: {
+  //   rejectUnauthorized: false,
+  //   ciphers: 'SSLv3'
+  // }
 };
