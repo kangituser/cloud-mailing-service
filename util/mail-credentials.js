7b7cdce55b4825c0eb233bfcc25b78de69a2f3da
kangituser <yehonatana@kan.org.il>
2020-12-07 13:52:50 +0200

    changing auth user

--- util/mail-credentials.js
+++ util/mail-credentials.js
@@ -1,5 +1,9 @@
 exports.credentials = {
   host: process.env.MAIL_HOST,
   port: 25,
-  secure: false
+  secure: false,
+  auth: {
+    user: 'jonathana@kan.org.il',
+    pass: 'cameljohn!1303'
+  }
 };
